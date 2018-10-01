b6ebd6b344cf6549a36b9be5b9bf8b2a653ecbbb
Wasif Khan <waasif.khaan@gmail.com>
2018-10-01 01:43:11 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-      @post.update(post_params)
-    if @post.valid?
-   
+    if @post.update(post_params)
+    
       redirect_to post_path(@post)
     else
       render :edit
